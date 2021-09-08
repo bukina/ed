c7edf12a7dc3505f3ff0c1587571b9e3f9878973
bukinatetina <bukinatet@gmail.com>
2021-09-08 18:48:45 +0300

    ed

---
+++ src/script.js
@@ -0,0 +1,62 @@
+"use strict";
+
+/*
+const obj = {
+    viktor: ['apple', 'grape', 'orange', 'apple', 'banana'],
+    kate: ['grape', 'orange', 'apple', 'grape', 'banana'],
+};
+
+
+function calc() {
+    // запрещено обращаться на прямую к obj.viktor или object.kate
+    
+   for (let key in obj){
+   if
+   
+   }
+    
+    // return ...
+}
+
+console.log(calc());
+
+//result of calc method should be equal to
+// {
+//     viktor: {
+//         apple: 2,
+//         grape: 1,
+//         orange: 1,
+//         banana: 1,
+//     },
+//     kate: {
+//         apple: 1,
+//         grape: 2,
+//         orange: 1,
+//         banana: 1,
+//     },
+// }
+
+
+let firstName = 'Tanya'
+
+let lastName = prompt('enter Last Name')
+alert(firstName + ' ' + lastName )
+
+let cars = ['df', 'ff', 'ffr']
+console.log(cars)
+
+let person = {
+    firstNam: 'Ta',
+    lastNam: 'bu',
+    age: 26
+
+}
+
+console.log(person)
+
+*/
+
+function getR(min, max){
+    return Math.floor(Math.random()*(max - min + 1)+ min)
+}
+console.log(getR(34,67))
